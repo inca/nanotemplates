2a031025a98caf218cf344a55b472dbb7eba6d9a
Boris Okunskiy <i@savant.pro>
2015-01-28 15:45:16 +0300

    Refactored tests

--- test/nano.js
+++ test/nano.js
@@ -2,12 +2,22 @@
 
 var Nano = require('../lib/nano')
   , fs = require('fs')
+  , path = require('path')
   , assert = require('assert');
 
 function assertHtml(actual, expected) {
   actual = actual.replace(/\s+</g, '<').replace(/>\s+/g, '>');
   expected = expected.replace(/\s+</g, '<').replace(/>\s+/g, '>');
   assert.equal(actual, expected);
+}
+
+function assertHtmlFile(actual, expectedFile, cb) {
+  fs.readFile(path.join(__dirname, 'templates', expectedFile), 'utf-8',
+    function(err, expected) {
+      if (err) return cb(err);
+      assertHtml(actual, expected);
+      cb();
+    });
 }
 
 describe('Nano', function() {
@@ -20,12 +30,25 @@ describe('Nano', function() {
 
     nano.render('simpleIncludes/index.html', function(err, html) {
       if (err) return done(err);
-      fs.readFile(__dirname + '/templates/simpleIncludes/_result.html', 'utf-8',
-        function(err, expected) {
-          if (err) return next(err);
-          assertHtml(html, expected);
-          done();
-        });
+      assertHtmlFile(html, 'simpleIncludes/_index.html', done);
+    });
+
+  });
+
+  it('should process simple layouts', function(done) {
+
+    nano.render('layouts/index.html', function(err, html) {
+      if (err) return done(err);
+      assertHtmlFile(html, 'layouts/_index.html', done);
+    });
+
+  });
+
+  it('should process layouts with block redifinition', function(done) {
+
+    nano.render('layouts/users/list.html', function(err, html) {
+      if (err) return done(err);
+      assertHtmlFile(html, 'layouts/users/_list.html', done);
     });
 
   });
