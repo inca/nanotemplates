4329e3c884b63fcea6c61b0ce339a52afa7382ff
Boris Okunskiy <i@savant.pro>
2015-01-28 15:34:37 +0300

    Implemented base grammar

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    peg: {
+      compile: {
+        src: "grammar/template.peg",
+        dest: "lib/parser.js"
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-peg');
+
+  grunt.registerTask('default', [ 'peg' ]);
+
+};
--- test/nano.js
+++ test/nano.js
@@ -16,4 +16,18 @@ describe('Nano', function() {
     basedir: __dirname + '/templates'
   });
 
+  it('should process simple includes', function(done) {
+
+    nano.render('simpleIncludes/index.html', function(err, html) {
+      if (err) return done(err);
+      fs.readFile(__dirname + '/templates/simpleIncludes/_result.html', 'utf-8',
+        function(err, expected) {
+          if (err) return next(err);
+          assertHtml(html, expected);
+          done();
+        });
+    });
+
+  });
+
 });
